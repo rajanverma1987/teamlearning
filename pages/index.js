2922595780b73687b291fe6e69688a330a7125df
rajan vermam <rajanverma1987@gmail.com>
2023-08-07 11:25:44 -0600

    Index page working

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,26 @@ import styles from "@/styles/Home.module.css";
 import { useState } from "react";
 
 export default function Home() {
-	// const [value, setValue] = useState(0);
+	const [value, setValue] = useState(0);
 
-	// function addValue() {
-	// 	setValue(prev => prev + 1);
-	// }
-	// function minusValue() {
-	// 	setValue(oldValue => --oldValue);
-	// }
+	function addValue() {
+		setValue(prev => prev + 1);
+	}
+	function minusValue() {
+		setValue(oldValue => --oldValue);
+	}
 
-	// return (
-	// 	<>
-	// 		<button className={styles.button} onClick={minusValue}>
-	// 			Minus
-	// 		</button>
-	// 		<p className={styles.text}>{value}</p>
-	// 		<button className={styles.button} onClick={addValue}>
-	// 			Plus
-	// 		</button>
-	// 	</>
-	// );
+	return (
+		<>
+			<button className={styles.button} onClick={minusValue}>
+				Minus
+			</button>
+			<p className={styles.text}>{value}</p>
+			<button className={styles.button} onClick={addValue}>
+				Plus
+			</button>
+		</>
+	);
 
 	return <></>;
 }
